02b3e8348fac08bb6f9ea21c21427e433adf9f61
mikoloism <mikoloism@gmail.com>
2024-02-12 14:41:28 +0330

    feat(pages): add build information to setting page

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,5 +1,18 @@
-import { IonContent, IonHeader, IonIcon, IonItem, IonList, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
+import { Device, type DeviceInfo } from '@capacitor/device';
+import {
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonList,
+  IonNote,
+  IonPage,
+  IonText,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import { informationCircleOutline } from 'ionicons/icons';
+import React from 'react';
 import { withTranslation, type WithTranslation } from 'react-i18next';
 import { I18nScope } from '../libs/i18n/mod';
 import { SettingsLanguageSelection } from '../libs/settings/mod';
@@ -23,9 +36,26 @@ const SettingsPage: React.FC<WithTranslation> = ({ t }) => (
           <IonIcon slot="start" icon={informationCircleOutline} />
           <IonText>{t('about_link_text')}</IonText>
         </IonItem>
+        <SettingsBuildInformation />
       </IonList>
     </IonContent>
   </IonPage>
 );
+
+function SettingsBuildInformation() {
+  const [device, setDeviceInfo] = React.useState<DeviceInfo>();
+
+  React.useEffect(function onMount() {
+    Device.getInfo().then(function subscribeInfo(deviceInfo) {
+      setDeviceInfo(() => deviceInfo);
+    });
+  }, []);
+
+  return (
+    <IonNote color="medium" style={{ textAlign: 'center' }}>
+      Zolfinak for {device?.model} version {device?.osVersion}
+    </IonNote>
+  );
+}
 
 export default withTranslation(I18nScope.SETTINGS)(SettingsPage);
