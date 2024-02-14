c8d6fea948fd499616d7b96fb086543df31e5f37
mikoloism <mikoloism@gmail.com>
2024-02-14 14:02:10 +0330

    perf(theme): improve seperate browser support
    the `.addListener` method was deprecated
    reference:
    https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList/addListener
    
    recommended to use `.addEventListener`
    	with `change` event
    also, I was saw an snippet from stackoverflow
    reference:
    https://stackoverflow.com/a/60000747/13417667
    
    I inhanced the `initilizeThemeSettings`
    	by seperate different Browser supportation
    	and resolve deprecation also

--- src/libs/themes/mod.ts
+++ src/libs/themes/mod.ts
@@ -3,14 +3,28 @@ import { StatusBar, Style } from '@capacitor/status-bar';
 export function initilizeThemeSettings(): void {
   if (typeof window === 'undefined') return;
 
-  window
-    .matchMedia('(prefers-color-scheme: dark)')
-    .addEventListener('change', function onPrefersColorScheme(status): void {
-      try {
-        StatusBar.setStyle({
-          style: status.matches ? Style.Dark : Style.Light,
-        });
-        // eslint-disable-next-line no-empty
-      } catch {}
+  const darkMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+  function onPrefersColorScheme(status: MediaQueryListEvent): void {
+    StatusBar.setStyle({
+      style: status.matches ? Style.Dark : Style.Light,
     });
+  }
+
+  function tryChromeandFirefox() {
+    darkMediaQuery.addEventListener('change', onPrefersColorScheme);
+  }
+
+  function trySafari() {
+    darkMediaQuery.addListener(onPrefersColorScheme);
+  }
+
+  try {
+    tryChromeandFirefox();
+  } catch {
+    try {
+      trySafari();
+      // eslint-disable-next-line no-empty
+    } catch {}
+  }
 }
