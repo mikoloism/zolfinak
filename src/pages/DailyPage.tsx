73bc47c8bd18a53aaaaa2930979a0455bd07aa2d
mikoloism <mikoloism@gmail.com>
2024-02-15 16:04:28 +0330

    feat(daily): add `Picker` component to `DailyPage`

--- src/pages/DailyPage.tsx
+++ src/pages/DailyPage.tsx
@@ -1,14 +1,33 @@
-import { IonText } from '@ionic/react';
+import { IonDatetimeButton, IonText, IonToolbar } from '@ionic/react';
 import type { WithTranslation } from 'react-i18next';
+import { DATE_PICKER_ID, DailyPicker } from '../libs/daily/Picker';
 import { I18nScope } from '../libs/i18n/token';
 import { Roller, withIonPageLayout, withPage, withTranslation } from '../libs/roll/mod';
+import { useDateStore } from '../libs/storage/date';
+
+function Toolbar(): JSX.Element {
+  const $date = useDateStore();
+
+  return (
+    <IonToolbar className="ion-padding">
+      <IonDatetimeButton datetime={DATE_PICKER_ID} is="ion-title" color={'medium'}>
+        {$date.formatted}
+      </IonDatetimeButton>
+    </IonToolbar>
+  );
+}
 
 function DailyPage() {
-  return <IonText is="h1">Daily(WIP)</IonText>;
+  return (
+    <>
+      <DailyPicker />
+      <IonText is="h1">Daily(WIP)</IonText>
+    </>
+  );
 }
 
 export default new Roller()
   .roll(withPage('/daily'))
   .roll<WithTranslation>(withTranslation(I18nScope.HOME))
-  .roll(withIonPageLayout('', { contentClassName: 'ion-padding', withToolbar: false }))
+  .roll(withIonPageLayout('', { Toolbar, contentClassName: 'ion-padding' }))
   .around(DailyPage);
