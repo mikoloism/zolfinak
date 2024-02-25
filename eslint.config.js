fc76bcf49c982189954c8f11c6d26b8c300cbb3c
mikoloism <mikoloism@gmail.com>
2024-02-25 17:35:01 +0330

    style(eslint): configuration
    - update: `eslint.config.js`
    	- add `typescript-eslint` rule
    	- add `import` rule
    - add: `pretter` inside `package.json`

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,84 @@
-import antfu from "@antfu/eslint-config";
-import * as ionicPrettierConfig from "@ionic/prettier-config";
+import antfu from '@antfu/eslint-config';
+import * as ionicPrettierConfig from '@ionic/prettier-config';
 
 export default antfu({
   test: true,
+
   typescript: {
-    tsconfigPath: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigPath: ['./tsconfig.json', './tsconfig.node.json'],
+    parserOptions: { project: ['./tsconfig.json', './tsconfig.node.json', './vite.config.ts', './eslint.config.js'] },
+    overrides: {
+      'ts/no-this-alias': 'off',
+      'ts/explicit-module-boundary-types': 'off',
+      'ts/consistent-generic-constructors': 'error',
+      'ts/adjacent-overload-signatures': 'warn',
+      'ts/consistent-type-assertions': 'error',
+      'ts/prefer-for-of': 'error',
+      'ts/prefer-optional-chain': 'error',
+      'ts/explicit-function-return-type': 'off',
+      'ts/prefer-readonly': 'error',
+      'ts/no-explicit-any': ['error', { ignoreRestArgs: true }],
+      'ts/unified-signatures': ['warn', { ignoreDifferentlyNamedParameters: true }],
+      'ts/array-type': ['error', { default: 'array-simple', readonly: 'array-simple' }],
+      'ts/consistent-type-imports': ['error', { prefer: 'type-imports', fixStyle: 'inline-type-imports' }],
+    },
   },
-  react: true,
+
+  react: {
+    overrides: {
+      'react/prop-types': 'off',
+      'react/jsx-sort-props': 'error',
+      'react-refresh/only-export-components': 'off',
+    },
+  },
+
+  javascript: {
+    overrides: {
+      'no-fallthrough': 'off',
+      'no-constant-condition': 'off',
+    },
+  },
   jsx: true,
 
+  stylistic: false,
   formatters: {
-    css: "prettier",
-    html: "prettier",
-    markdown: "prettier",
+    css: 'prettier',
+    html: 'prettier',
+    markdown: 'prettier',
     prettierOptions: ionicPrettierConfig,
-  },
-  stylistic: {
-    jsx: true,
-    semi: true,
-    quotes: "single",
-    indent: 4,
   },
 
   jsonc: false,
   yaml: false,
   gitignore: true,
 
-  files: ["src/**/*.ts", "src/**/*.tsx"],
+  files: ['src/**/*.ts', 'src/**/*.tsx'],
   ignores: [
-    "node_modules",
-    "**/*.config.{js,ts,mts,cts,mjs,cjs}",
-    "**/*.json",
-    "**/*.{yaml,yml}",
-    "**/*.d.ts",
-    "**/*.test.{ts,tsx,js,jsx}",
-    "cypress/**/*.*",
-    "tests/**/*.*",
+    'node_modules',
+    '**/*.config.{js,ts,mts,cts,mjs,cjs}',
+    '**/*.json',
+    '**/*.{yaml,yml}',
+    '**/*.d.ts',
+    '**/*.test.{ts,tsx,js,jsx}',
+    'cypress/**/*.*',
+    'tests/**/*.*',
   ],
+
+  rules: {
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'ignore',
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false,
+        },
+      },
+    ],
+  },
 });
