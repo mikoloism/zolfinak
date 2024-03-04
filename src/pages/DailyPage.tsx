4458df62d717501c93057d9864057baf6625265f
mikoloism <mikoloism@gmail.com>
2024-03-04 20:24:13 +0330

    feat(daily): add MonthView
    - commented currently another pieces of code
    
    to experiment and testing to create calendar for TZ's and Calendar System

--- src/pages/DailyPage.tsx
+++ src/pages/DailyPage.tsx
@@ -1,13 +1,16 @@
 import type { WithTranslation } from 'react-i18next';
-import { DailyPicker, DailyPickerButton } from '../libs/daily/Picker';
-import { I18nScope } from '../libs/i18n/token';
-import { Roller, withIonPageLayout, withPage, withTranslation } from '../libs/roll/mod';
+
+// import { DailyPicker, DailyPickerButton } from 'libs/daily/Picker';
+import { MonthView } from 'libs/calendar/components/MonthView';
+import { I18nScope } from 'libs/i18n/token';
+import { Roller, withIonPageLayout, withPage, withTranslation } from 'libs/roll/mod';
 
 function DailyPage() {
   return (
     <>
-      <DailyPickerButton />
-      <DailyPicker />
+      {/* <DailyPickerButton /> */}
+      {/* <DailyPicker /> */}
+      <MonthView />
     </>
   );
 }
