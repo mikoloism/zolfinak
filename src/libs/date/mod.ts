6470c0b42618f292b520ceb44cf1fecc261cd4ee
mikoloism <mikoloism@gmail.com>
2024-03-05 20:09:23 +0330

    feat(date): init `date` scope to extend `dayjs`
    the `libs/date/` contain date and time libraries
    as module (high-level module)
    
    wrap `dayjs` with essential built-in plugins and
    `jalaliday` plugin
    
    import essential locales `en` and `fa`

---
+++ src/libs/date/mod.ts
@@ -0,0 +1,26 @@
+import dayjs from 'dayjs';
+import calendar from 'dayjs/plugin/calendar';
+import isBetween from 'dayjs/plugin/isBetween';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import isToday from 'dayjs/plugin/isToday';
+import toObject from 'dayjs/plugin/toObject';
+import updateLocale from 'dayjs/plugin/updateLocale';
+import weekday from 'dayjs/plugin/weekday';
+import jalaliday from 'jalaliday';
+
+import 'dayjs/locale/en';
+import 'dayjs/locale/fa';
+
+dayjs.extend(calendar);
+dayjs.extend(isBetween);
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isToday);
+dayjs.extend(toObject);
+dayjs.extend(updateLocale);
+dayjs.extend(weekday);
+dayjs.extend(jalaliday);
+
+export type { Dayjs, FormatObject } from 'dayjs';
+export { dayjs };
