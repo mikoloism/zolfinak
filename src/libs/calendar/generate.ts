ec0c1d875f436fc6fd07ad2ac2090ba020130bce
mikoloism <mikoloism@gmail.com>
2024-03-05 20:16:43 +0330

    feat(calendar): rewrite in `dayjs` to generate `month-view`

---
+++ src/libs/calendar/generate.ts
@@ -0,0 +1,18 @@
+import type { Dayjs } from 'lib/date/mod';
+
+function month_view(date: Dayjs, startDayOfWeek: number = 0): Dayjs[] {
+  const clonedDate = date.clone();
+  const startPointOfMonth = clonedDate.startOf('months').startOf('weeks').startOf('days');
+  const endPointOfMonth = clonedDate.endOf('months').endOf('weeks').startOf('days');
+
+  let crawlPoint = startPointOfMonth.subtract(startDayOfWeek, 'days');
+  let daysOfMonth: Dayjs[] = [];
+  while (endPointOfMonth.isSame(crawlPoint) === false) {
+    crawlPoint = crawlPoint.clone().add(1, 'day');
+    daysOfMonth = daysOfMonth.concat(crawlPoint);
+  }
+
+  return daysOfMonth;
+}
+
+export { month_view as generateMonthView };
