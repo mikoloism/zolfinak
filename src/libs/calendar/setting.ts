7274c3f82908c91685eea15940db8c2b7af34b52
mikoloism <mikoloism@gmail.com>
2024-03-06 12:44:11 +0330

    perf(settings): implement `DEFAULT_VALUE` for `CalendarType` and `LanguageCode`

--- src/libs/calendar/setting.ts
+++ src/libs/calendar/setting.ts
@@ -1,6 +1,9 @@
 import { IndividualSetting } from 'lib/settings/base';
+import { Calendar } from './calendar';
 
-export class CalendarTypeSetting extends IndividualSetting<string> {
+export class CalendarTypeSetting extends IndividualSetting<Calendar> {
+  public readonly DEFAULT_VALUE: Calendar = Calendar.JALALI;
+
   public constructor() {
     super();
   }
--- src/libs/i18n/setting.ts
+++ src/libs/i18n/setting.ts
@@ -1,7 +1,21 @@
 import { IndividualSetting } from 'lib/settings/base';
+import { getLocaleCode } from './helpers';
+import { LanguageName } from './languages';
 
 export class LanguageCodeSetting extends IndividualSetting<string> {
+  public readonly DEFAULT_VALUE: string;
+
   public constructor() {
     super();
+
+    this.DEFAULT_VALUE = getLocaleCode(LanguageName.DEFAULT);
   }
+
+  public override get = (): string => {
+    return getLocaleCode(this._value as LanguageName) ?? this.DEFAULT_VALUE;
+  };
+
+  public override update = (value: string): void => {
+    this._value = value;
+  };
 }
