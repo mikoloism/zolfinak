3caf8b6ca430646f00fd252f9e55e73e6827be02
mikoloism <mikoloism@gmail.com>
2024-03-07 15:48:03 +0330

    fix(calendar): rewrite `mont-view` component
    to use new `dayjs` and calendar generation of `month-view`
    tables.
    
    NOTE: this edition is just initly write!

--- src/libs/calendar/components/MonthView.tsx
+++ src/libs/calendar/components/MonthView.tsx
@@ -1,20 +1,50 @@
-import { TimeZone } from 'lib/timezone/timezone';
 import { Calendar } from 'lib/calendar/calendar';
-import { PersianCalendar } from '../persian';
+import { getTextDirection } from 'lib/i18n/helpers';
+import { i18n } from 'lib/i18n/i18n';
+import type { LanguageName } from 'lib/i18n/languages';
+import { TimeZone } from 'lib/timezone/timezone';
+import styled from 'styled-components';
 
 
 export const DEFAULT_TIMEZONE = TimeZone['Asia/Tehran'];
+const WEEKDAY_NAMES = [
+  'شنبه',
+  'یکشنبه',
+  'دوشنبه',
+  'سه‌شنبه',
+  'چهارشنبه',
+  'پنجشنبه',
+  'جمعه',
+];
 
-export function MonthView(): JSX.Element {
-  const weeksInMonth = new PersianCalendar().generate()
-
-  return <div>
-    {weeksInMonth.map(function crawl_month(date, index) {
-      return <div key={index}>
-        {date.format('DD MMMM')}
-      </div>
-    })}
-  </div>;
+export function WeekDayNameBar(): JSX.Element {
+  return <ul className='flex flex-(row wrap place-items-center place-content-around) w-full'>
+    {WEEKDAY_NAMES.map((weekDayName) => (<li
+      className='flex flex-(col wrap place-items-center place-content-center) font-300  text-sky-300 bg-sky-600 bg-opacity-30 w-1/7 p3 '
+      key={weekDayName}>{weekDayName}</li>))}
+  </ul>
 }
 
+export const DaysGrid = styled.div`
+  grid-template-columns: repeat(7, 1fr);
+`
+
+export function MonthView(): JSX.Element {
+  const weeksInMonth = Calendar.generateOf();
+
+  return <div className='w-[600px]'>
+    <WeekDayNameBar />
+    <div className='grid grid-(place-content-center place-items-center)' style={{
+      direction: getTextDirection(i18n.language as LanguageName) as never,
+      gridTemplateColumns: 'repeat(7, 1fr)',
+      gridTemplateRows: 'repeat(7, 1fr)',
+    }}>
+      {weeksInMonth.map(function crawl_month(date, index) {
+        return <button className='w-full h-full flex flex-(col wrap place-items-center place-content-center) p-4 text-center' key={index}>
+          {date.format('DD')}
+        </button>
+      })}
+    </div>
+  </div>;
+}
 
