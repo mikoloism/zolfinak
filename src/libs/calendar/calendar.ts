dd63ad172389d78ba04d4e8bf0a82a34a84e3f66
mikoloism <mikoloism@gmail.com>
2024-03-08 21:02:00 +0330

    perf(calendar): support `timezone` in generator

--- src/libs/calendar/calendar.ts
+++ src/libs/calendar/calendar.ts
@@ -1,6 +1,7 @@
 import { dayjs, type Dayjs } from 'lib/date/mod';
 import { globalSetting } from 'lib/settings/global';
 import { generateMonthView, type DaysInMonth } from './generator';
+import { TimeZone } from '../timezone/timezone';
 
 enum Calendar {
   GREGORIAN = 'gregory',
@@ -14,8 +15,9 @@ namespace Calendar {
   export function generateOf(today?: Date | Dayjs): DaysInMonth {
     const localeName = globalSetting.language.get();
     const calendarType = globalSetting.calendar.get();
-    const date = dayjs(today).calendar(calendarType).locale(localeName);
-    return generateMonthView(date);
+    const firstDayOfWeek = globalSetting.firstDayOfWeek.get();
+    const date = dayjs(today).calendar(calendarType).locale(localeName).tz(TimeZone['Asia/Tehran']);
+    return generateMonthView(date, firstDayOfWeek);
   }
 }
 
