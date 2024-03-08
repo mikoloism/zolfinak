2bf846d8ff3f9940d4fcbaf6b67c787b158b684b
mikoloism <mikoloism@gmail.com>
2024-03-08 21:01:12 +0330

    perf(calendar): support first day of week in generator

--- src/libs/calendar/generator.ts
+++ src/libs/calendar/generator.ts
@@ -1,13 +1,15 @@
+import { WeekDay } from 'lib/calendar/weekday';
 import type { Dayjs } from 'lib/date/mod';
 
 export type DaysInMonth = Dayjs[];
 
-function month_view(date: Dayjs, startDayOfWeek: number = 0): Dayjs[] {
-  const clonedDate = date.clone();
-  const startPointOfMonth = clonedDate.startOf('months').startOf('weeks').startOf('days');
-  const endPointOfMonth = clonedDate.endOf('months').endOf('weeks').startOf('days');
+function month_view(date: Dayjs, startDayOfWeek: WeekDay = WeekDay.DEFAULT): Dayjs[] {
+  const clonedDate: Dayjs = date.clone();
+  const startPointOfMonth: Dayjs = clonedDate.startOf('months').startOf('weeks').startOf('days');
+  const endPointOfMonth: Dayjs = clonedDate.endOf('months').endOf('weeks').startOf('days');
+  const firstDayOfWeek: number = (((startDayOfWeek - startPointOfMonth.day()) as unknown as number) + 7) % 7;
 
-  let crawlPoint = startPointOfMonth.subtract(startDayOfWeek, 'days');
+  let crawlPoint: Dayjs = startPointOfMonth.subtract(firstDayOfWeek, 'days');
   let daysOfMonth: Dayjs[] = [];
   while (endPointOfMonth.isSame(crawlPoint) === false) {
     crawlPoint = crawlPoint.clone().add(1, 'day');
