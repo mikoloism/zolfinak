164d0012e7e8c24ddbe34ddcf0599d82080d5e3a
mikoloism <mikoloism@gmail.com>
2024-03-08 20:55:03 +0330

    feat(date): support `timezone`
    extend `dayjs` with `utc` and `timezone`

--- src/libs/date/mod.ts
+++ src/libs/date/mod.ts
@@ -4,14 +4,18 @@ import isBetween from 'dayjs/plugin/isBetween';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 import isToday from 'dayjs/plugin/isToday';
+import timezone from 'dayjs/plugin/timezone';
 import toObject from 'dayjs/plugin/toObject';
 import updateLocale from 'dayjs/plugin/updateLocale';
+import utc from 'dayjs/plugin/utc';
 import weekday from 'dayjs/plugin/weekday';
 import jalaliday from 'jalaliday';
 
 import 'dayjs/locale/en';
 import 'dayjs/locale/fa';
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
 dayjs.extend(calendar);
 dayjs.extend(isBetween);
 dayjs.extend(isSameOrAfter);
